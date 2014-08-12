b7ffb1fa92af94b203ce85dcde49672b0bbbcf4d
n.wagensonner <n.wagensonner@voycer.com>
2014-08-12 10:04:36 +0200

    Add dependencies for new mac

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,3 +1,62 @@
+#settings
+echo "Input hostname for this machine"
+read hostname
+
+if [ -z "$hostname" ] ; then
+  echo "Hostname must be set."
+  exit 1
+fi 
+
+#create folder structure
+mkdir -p ~/Development/Go
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/undo
+
+#install homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew update
+
+#install brew dependencies
+brew install wget
+brew install mongodb
+brew install go
+brew install git
+brew install vim --override-system-vi --with-python3
+
+#install cask for brew
+brew install caskroom/cask/brew-cask
+
+#install closed source applications
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install dropbox
+brew cask install vlc
+
+#installing vim dependencies
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
 #go deps
 go get -v code.google.com/p/rog-go/exp/cmd/godef
 go get -v code.google.com/p/go.tools/cmd/cover
+
+#configure system
+sudo scutil --set ComputerName $hostname
+sudo scutil --set HostName $hostname
+sudo scutil --set LocalHostName $hostname
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $hostname
+
+#install configuration files
+cp bash_profile ~/.bash_profile
+
+#some mac specific settings
+
+#disable default save to cloud
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+#disable app quarantine
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+
